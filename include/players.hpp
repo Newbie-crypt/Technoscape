8a853b40817b2bd99b848d780bc9f3edd9f9a5f9
newbie-crypt <aliabohammar2@gmail.com>
2026-04-10 15:14:22 +0200

    Make the enemy exhibit some animation when it's idle

--- include/players.hpp
+++ include/players.hpp
@@ -5,6 +5,7 @@
 #include <QKeyEvent>
 #include <QObject>
 #include <QPainter>
+#include <QTimer>
 
 class Player : public QGraphicsRectItem {
     private:
@@ -41,10 +42,37 @@ class Enemy: public QGraphicsObject {
 
 class Robot: public Enemy {
     Q_OBJECT
+    private:
+        std::vector<QPixmap> idle_frames;
+        QPixmap idle_spritesheet;
+        int idle_frame_count = 4;
+        int current_idle_frame = 0;
     public:
-        Robot() : Enemy(100, ":/assets/Standing_Robot.png", 3) {}
+        Robot() : Enemy(100, ":/assets/Standing_Robot.png", 3) {
+
+            idle_spritesheet.load(":assets/OrangeRobot_Idle.png");
+
+            int idle_frame_width = idle_spritesheet.width() / 4;
+            int idle_frame_height = idle_spritesheet.height() / 2;
+
+            // Storing all the frames in one vector container
+            for (int i = 0; i < idle_frame_count; i++) {
+                idle_frames.push_back(idle_spritesheet.copy(i * idle_frame_height, 0, idle_frame_width, idle_frame_height));
+            }
+
+            QTimer* timer = new QTimer(this);
+
+            connect(timer, &QTimer::timeout, [this]() {
+                current_idle_frame = (current_idle_frame + 1) % idle_frame_count;
+                update(); // reconstruct the design.
+            });
+
+            timer->start(100);
+        }
+
+
         void paint(QPainter* painter, const QStyleOptionGraphicsItem*, QWidget*) override {
-            painter->drawPixmap(0, 0, sprite);
+            painter->drawPixmap(0, 0, idle_frames[current_idle_frame]);
         }
     public slots:
         void Move() override {moveBy(rand() % 20 - 10, rand() % 20 - 10);}
