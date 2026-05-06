9381f4943cad3e2b365baef7936944a4ec2b0e10
newbie-crypt <aliabohammar2@gmail.com>
2026-05-06 18:12:40 +0300

    Add all walls in Level 3 scene

--- src/levelThree.cpp
+++ src/levelThree.cpp
@@ -22,6 +22,23 @@ void levelThree::setupScene() {
     };
 
     addWall(235, 92, 127, 111);
+    addWall(235, 366, 138, 97);
+    addWall(383, 418, 37, 51);
+    addWall(244, 682, 140, 71);
+    addWall(403, 681, 34, 56);
+    addWall(1096, 327, 45, 46);
+    addWall(969, 131, 52, 73);
+    addWall(1086, 136, 51, 120);
+    addWall(1185, 213, 44, 114);
+    addWall(1243, 215, 55, 157);
+    addWall(1215, 534, 47, 81);
+    addWall(989, 529, 93, 73);
+    addWall(963, 561, 28, 57);
+    addWall(1030, 608, 25, 28);
+    addWall(1083, 546, 71, 61);
+    addWall(972, 836, 35, 85);
+    addWall(1135, 757, 106, 89);
+    addWall(1083, 781, 41, 55);
     addWall(383, 138, 33, 75);
     addWall(237, 561, 79, 52);
     addWall(211, 840, 172, 137);
@@ -30,6 +47,15 @@ void levelThree::setupScene() {
     addWall(237, 88, 182, 129);
     addWall(883, 148, 29, 61);
     addWall(971, 196, 131, 129);
+    addWall(0, 0, 1379, 65);
+    addWall(29, 67, 78, 1038);
+    addWall(108, 242, 38, 204);
+    addWall(111, 679, 28, 410);
+    addWall(88, 1035, 1223, 44);
+    addWall(1293, 928, 90, 179);
+    addWall(1329, 0, 73, 927);
+    addWall(1274, 51, 49, 124);
+    addWall(1293, 173, 31, 22);
 
     QPixmap health_symbol_image (":/assets/health_symbol.png");
     health_symbol_image = health_symbol_image.scaled(60, 60, Qt::KeepAspectRatio, Qt::SmoothTransformation);
